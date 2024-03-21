8bec48f446251436d95eaa1fa6520a6a89e98792
Joshua Dinh <josh@joshuadinh.tech>
2024-03-21 06:59:54 -0700

    feature: export block notice

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export {default as CurrencyControl} from "./CurrencyControl";
 export {default as ClassicEditor} from "./ClassicEditor";
 export {default as OptionsPanel} from "./OptionsPanel";
 export {default as SettingsSection} from "./SettingsSection";
+export {default as BlockNotice} from "./BlockNotice";
