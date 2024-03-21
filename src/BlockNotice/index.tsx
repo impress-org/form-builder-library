ca33d8bb702d9ae2f2bcacfcfbf9fc0eaad5040c
Joshua Dinh <josh@joshuadinh.tech>
2024-03-21 09:14:56 -0700

    refactor: include optional elements, adjust children prop position

--- src/BlockNotice/index.tsx
+++ src/BlockNotice/index.tsx
@@ -1,23 +1,25 @@
 import {ReactNode} from 'react';
-import "./styles.scss";
+import './styles.scss';
 
 type BlockNoticeProps = {
-    title: string,
-    description: string,
-    href: string
-    anchorText: string;
-    children?: ReactNode
-}
+    title: string;
+    description?: string;
+    href?: string;
+    anchorText?: string;
+    children?: ReactNode;
+};
 
 export default function BlockNotice({title, description, href, anchorText, children}: BlockNoticeProps) {
     return (
         <div className={'givewp-block-notice'}>
             <span className={'givewp-block-notice__title'}>{title}</span>
-            <span className={'givewp-block-notice__description'}>{description}</span>
-            <a className={'givewp-block-notice__anchor'} href={href} target={'_blank'} rel={'noreferrer noopener'} >
-                {anchorText}
-            </a>
+            {description && <span className={'givewp-block-notice__description'}>{description}</span>}
             {children}
+            {href && (
+                <a className={'givewp-block-notice__anchor'} href={href} target={'_blank'} rel={'noreferrer noopener'}>
+                    {anchorText}
+                </a>
+            )}
         </div>
     );
 }
