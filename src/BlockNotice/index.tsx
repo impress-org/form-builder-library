a4d0c9563c0a80becbeab0e30a86b79780f91bba
Joshua Dinh <josh@joshuadinh.tech>
2024-03-21 08:45:04 -0700

    refactor: include missing anchor text

--- src/BlockNotice/index.tsx
+++ src/BlockNotice/index.tsx
@@ -5,15 +5,18 @@ type BlockNoticeProps = {
     title: string,
     description: string,
     href: string
+    anchorText: string;
     children?: ReactNode
 }
 
-export default function BlockNotice({title, description, href, children}: BlockNoticeProps) {
+export default function BlockNotice({title, description, href, anchorText, children}: BlockNoticeProps) {
     return (
         <div className={'givewp-block-notice'}>
             <span className={'givewp-block-notice__title'}>{title}</span>
             <span className={'givewp-block-notice__description'}>{description}</span>
-            <a className={'givewp-block-notice__anchor'} href={href} target={'_blank'} rel={'noreferrer noopener'} />
+            <a className={'givewp-block-notice__anchor'} href={href} target={'_blank'} rel={'noreferrer noopener'} >
+                {anchorText}
+            </a>
             {children}
         </div>
     );
