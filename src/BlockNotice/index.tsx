591577ca7f0abdf89f8512c612d437b69f5ded1c
Joshua Dinh <josh@joshuadinh.tech>
2024-03-21 08:39:06 -0700

    feature: include children prop

--- src/BlockNotice/index.tsx
+++ src/BlockNotice/index.tsx
@@ -1,17 +1,20 @@
+import {ReactNode} from 'react';
 import "./styles.scss";
 
 type BlockNoticeProps = {
     title: string,
     description: string,
     href: string
+    children?: ReactNode
 }
 
-export default function BlockNotice({title, description, href}: BlockNoticeProps) {
+export default function BlockNotice({title, description, href, children}: BlockNoticeProps) {
     return (
         <div className={'givewp-block-notice'}>
             <span className={'givewp-block-notice__title'}>{title}</span>
             <span className={'givewp-block-notice__description'}>{description}</span>
             <a className={'givewp-block-notice__anchor'} href={href} target={'_blank'} rel={'noreferrer noopener'} />
+            {children}
         </div>
     );
 }
