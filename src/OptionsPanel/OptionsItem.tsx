336dabbd5d5143f4f5f106d1fa68fc9e1137f143
Joshua Dinh <josh@joshuadinh.tech>
2024-04-23 09:48:36 -0700

    refactor: remove label handler from currency control

--- src/OptionsPanel/OptionsItem.tsx
+++ src/OptionsPanel/OptionsItem.tsx
@@ -61,7 +61,6 @@ export default function OptionsItem({
                             hideLabelFromVision
                             value={option.value}
                             onValueChange={(value) => {
-                                handleUpdateOptionLabel(value);
                                 handleUpdateOptionValue(value);
                             }}
                         />
