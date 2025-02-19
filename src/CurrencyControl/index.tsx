60e60bcf2cb9d72bee21731607b01bbae96c1951
Joshua Dinh <75056371+JoshuaHungDinh@users.noreply.github.com>
2025-02-19 13:37:05 -0800

    Fix: disable currency abbreviations (#13)
    * fix: disable currency abbreviations. replace non-breaking-spaces
    
    * refactor: separate number formatting parts into own function

--- src/CurrencyControl/index.tsx
+++ src/CurrencyControl/index.tsx
@@ -4,7 +4,7 @@ import {useInstanceId} from '@wordpress/compose';
 import {useEffect, useState} from '@wordpress/element';
 import {BaseControl} from '@wordpress/components';
 import {CurrencyCode} from './CurrencyCode';
-import parseValueFromLocale from './parseValueFromLocale';
+import parseValueFromLocale, {getNumberFormattingParts} from './parseValueFromLocale';
 
 interface CurrencyControlProps extends CurrencyInputProps {
     currency: CurrencyCode;
@@ -12,6 +12,7 @@ interface CurrencyControlProps extends CurrencyInputProps {
     hideLabelFromVision?: boolean;
     help?: string;
 }
+
 
 export default function CurrencyControl({
     label,
@@ -24,14 +25,16 @@ export default function CurrencyControl({
 }: CurrencyControlProps) {
     const [localizedValue, setLocalizedValue] = useState(value);
 
-     useEffect(() => {
-        if (value !== localizedValue){
+    useEffect(() => {
+        if (value !== localizedValue) {
             setLocalizedValue(value);
         }
     }, [value]);
 
     // simplified implementation of useBaseControlProps()
     const uniqueId = useInstanceId(BaseControl, 'wp-components-base-control');
+
+    const {groupSeparator, decimalSeparator} = getNumberFormattingParts();
 
     const updateValue = (value: string) => {
         setLocalizedValue(value);
@@ -42,6 +45,15 @@ export default function CurrencyControl({
         <BaseControl label={label} help={help} id={uniqueId} hideLabelFromVision={hideLabelFromVision}>
             <CurrencyInput
                 id={uniqueId}
+                disableAbbreviations
+                decimalSeparator={decimalSeparator}
+                groupSeparator={
+                    /**
+                     * Replace non-breaking space to avoid conflict with the suffix separator.
+                     * @link https://github.com/cchanxzy/react-currency-input-field/issues/266
+                     */
+                    groupSeparator.replace(/\u00A0/g, ' ')
+                }
                 value={localizedValue}
                 onValueChange={updateValue}
                 className={'components-text-control__input'}
